dceed1ce652c7248b076d9c2daab1c841356d0b4
Yaron Hayardeni <yaronHayar@gmail.com>
2022-07-15 13:06:26 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: './docs'
-  }
+		outDir: './docs', // for github pages
+	},
+    base: '/Torus/',
 })
