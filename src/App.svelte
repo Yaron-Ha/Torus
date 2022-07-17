5f61c7fab398857b44523ad9c84e9505862c5c3b
Yaron Hayardeni <yaronHayar@gmail.com>
2022-07-17 11:31:12 +0300

    Done

--- src/App.svelte
+++ src/App.svelte
@@ -15,21 +15,21 @@
 </script>
 
 <main>
-	​<SC.Canvas antialias background={new THREE.Color('floralwhite')}>
+	​<SC.Canvas antialias background={new THREE.Color(0x080808)}>
 		<SC.Mesh
 			geometry={new THREE.TorusGeometry(radius, thickness, 32, 100)}
-			rotation={[spin, spin, spin]}
-			material={new THREE.MeshStandardMaterial({ color: 0x8b8b83 })}
+			rotation={[0 + spin, 7 + spin, 0 + spin]}
+			material={new THREE.MeshBasicMaterial({ color: 0xfcfcfc })}
 		/>
-		<SC.PerspectiveCamera position={[25, 1, 25]} />
+		<SC.PerspectiveCamera position={[25, 0, 25]} />
 		<SC.OrbitControls enableZoom={false} />
 		​</SC.Canvas
 	>
 </main>
 
 <div class="controls">
-	<label><input type="range" bind:value={thickness} min={0.5} max={3} step={0.05} /> thickness</label>
-	<label><input type="range" bind:value={radius} min={1} max={10} step={0.1} /> radius</label>
+	<label><input type="range" bind:value={thickness} min={0} max={3} step={0.01} /> thickness</label>
+	<label><input type="range" bind:value={radius} min={0} max={10} step={0.1} /> radius</label>
 	<label><input type="checkbox" bind:checked={isPaused} selected /> pause</label>
 </div>
 
